3ee80e8042c1d32fc70d170a828852b12d148a97
Xpop360 <barreiro.pedro7@gmail.com>
2018-06-12 19:39:37 +0100

    base

---
+++ SQL_Project/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace SQL_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quintaDeBatatasDataSet = new SQL_Project.QuintaDeBatatasDataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.quintaDeBatatasDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // quintaDeBatatasDataSet
+            // 
+            this.quintaDeBatatasDataSet.DataSetName = "QuintaDeBatatasDataSet";
+            this.quintaDeBatatasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(753, 411);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.quintaDeBatatasDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private QuintaDeBatatasDataSet quintaDeBatatasDataSet;
+    }
+}
+
---
+++ SQL_Project/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_Project
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=DESKTOP-IQR808E;Initial Catalog=QuintaDeBatatas;Integrated Security=True";
+
+        SqlConnection connect;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SqlConnection connect = new SqlConnection(connectionString);
+            connect.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
